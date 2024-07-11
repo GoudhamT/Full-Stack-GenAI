24e351c050676331236cd9434f2b38edd613666c
GoudhamT <goudhamvel.88@gmail.com>
2024-07-11 09:57:57 +0530

    SQL joins part2

--- SQL/Practice/Joins_queries.sql
+++ SQL/Practice/Joins_queries.sql
@@ -106,3 +106,43 @@ left outer join customer as c
 on s.salesman_id = c.salesman_id
 order by salesman_id; 
 
+
+#13. From the following tables write a  SQL query to list all salespersons along with customer name, city, grade, order number, date, and amount. 
+#Condition for selecting list of salesmen : 1. Salesmen who works for one or more customer or,
+# 2. Salesmen who not yet join under any customer, Condition for selecting list of customer :
+# 3. placed one or more orders, or 4. no order placed to their salesman.
+
+select ord_no, ord_date, purch_amt, cust_name,grade,c.city, s.salesman_id,s.name as Salesperson, s.city, commission
+from orders 
+right outer join customer as c
+on orders.customer_id = c.customer_id
+right outer join salesman as s
+on c.salesman_id = s.salesman_id;
+
+# 14. Write a  SQL statement to make a list for the salesmen who either work for one or more customers or yet to join any of the customer.
+# The customer may have placed, either one or more orders on or above order amount 2000 and must have a grade, 
+# or he may not have placed any order to the associated supplier.
+
+select c.customer_id, cust_name, c.city , grade, s.name as Salesperson , s.city, ord_no,ord_date,purch_amt
+from customer as c
+right outer join salesman as s
+on s.salesman_id = c.salesman_id
+left outer join orders as o
+on  o.customer_id = c.customer_id 
+where o.purch_amt > 250
+and c.grade is not null;
+
+# 5.For those customers from the existing list who put one or more orders, or which orders have been placed by the customer who is not on the list, 
+#create a report containing the customer name, city, order number, order date, and purchase amount
+
+select cust_name, c.city, ord_no , ord_date , purch_amt from customer  as c
+right outer join orders 
+on c.customer_id = orders.customer_id; 
+
+# 16. Write a  SQL statement to generate a report with the customer name, city, order no. order date, purchase amount for only those customers on the list who must have a grade and placed one or more orders 
+#or which order(s) have been placed by the customer who neither is on the list nor has a grade.
+select c.customer_id,cust_name, c.city , ord_no , ord_date, purch_amt, o.customer_id from customer  as c
+right outer join orders as o
+on c.customer_id = o.customer_id 
+where c.grade is not null;
+
